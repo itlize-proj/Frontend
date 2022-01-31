9ed2899b4772e869e4989541f1fb0a9b51cd7fe0
Yohannes Habte <yohannes.habte@mnsu.edu>
2022-01-31 16:19:17 -0600

    initial setup libraries

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import { Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
